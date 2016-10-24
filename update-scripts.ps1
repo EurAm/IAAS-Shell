c9bddec6e9c050e71529b17c6bde29990da7f113
Tom Allard <tom.allard@euram.eu>
2016-10-24 14:53:08 +0200

    Add self-update script

---
+++ update-scripts.ps1
@@ -0,0 +1,31 @@
+﻿Param(
+    [string]$workDir = (get-location)   ,
+    [string]$source = "https://github.com/EurAm/IAAS-Shell/archive/master.zip"
+)
+If ((Get-Module "common").Count -gt 0) {
+    Remove-Module "common"
+}
+
+Import-Module ./common.psm1
+
+$currentDirName = [System.IO.Path]::GetFileName($workDir)
+if ($currentDirName -ieq "Scripts"){
+    $workDir = [System.IO.Path]::GetDirectoryName($workDir)
+}
+
+
+$imagesDirName = "VHDs"
+$scriptsDirName = "Scripts"
+$tempDirName = "Temp"
+
+$tempDir = Join-Path $workDir $tempDirName
+$scriptsDir = Join-Path $workDir $scriptsDirName
+$zip = Join-Path $tempDir "master.zip"
+DownloadFile $source $zip
+$now = [System.DateTime]::Now.ToString().Replace(":", "").Replace("/","")
+$tempunpackdir = Join-Path $tempDir $now
+TryCreateDir $tempunpackdir
+[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zip, $tempunpackdir)
+$cdir =  (Get-ChildItem $tempunpackdir).Name
+cp (Join-Path $tempunpackdir "$cdir\*") $scriptsDir -Recurse -Force -Exclude InstallScripts
